ff6f7c890296b2aab83497ac561d6fcf92a5a40c
Garrett Oreilly <garrettoreilly@icloud.com>
2014-10-21 23:58:30 -0400

    Reads in rubik's cube and stores it

--- solvable.c
+++ solvable.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include "solvable.h"
 
-// Definitions
-#define GOAL "RRRRRRRRRYYYYYYYYYBBBBBBBBBOOOOOOOOOWWWWWWWWWGGGGGGGGG"
-char xcoor[54] = {0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,0,0,0,2,2,2,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0};
-char ycoor[54] = {0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,1,1,1,2,2,2,2,1,0,2,1,0,2,1,0};
-char zcoor[54] = {0,0,0,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,0,2,1,0,2,1,0,2,1,0,2,1,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,2,2,2};
+// Bit values of colors
+unsigned char none = 0;
+unsigned char red = 1;
+unsigned char green = 2;
+unsigned char yellow = 3;
+unsigned char blue = 4;
+unsigned char orange = 5;
+unsigned char white = 6;
 
-// Prototypes
-void returnFalse();
-void returnTrue();
-char * findConnectedTile(int x, int y, int z);
-void findHome(char * connectedTiles, int result[]);
+// Used to determine value of Z-axis
+unsigned char adjacency[6][4] = {{2,3,4,6},{1,3,5,6},{1,2,4,5},{1,3,5,6},{2,3,4,6},{1,2,4,5}};
+
+// Solved cube
+unsigned char goalState[20] = {70,7,134,68,132,69,5,133,67,131,65,129,86,23,150,84,148,85,21,149};
 
 int main(int argc, char *argv[]) {
 
@@ -27,52 +32,52 @@ int main(int argc, char *argv[]) {
     if (input == 0) {
         returnFalse();
     }
-    
     // Save the orientation of the cube from the file to a string
-    int c;
-    int count = 0;
-    char cube[55] = {'\0'};
-    while ((c = fgetc(input)) != EOF && count < 54) {
-        if (c != '\n' && c != ' ') {
-            cube[count] = c;
-            count++;
+    int t;
+    int tilesCount = 0;
+    char tiles[55];
+    while ((t = fgetc(input)) != EOF && tilesCount < 54) {
+        if (t != '\n' && t != ' ') {
+            tiles[tilesCount] = t;
+            tilesCount++;
         }
     }
+    tiles[54] = '\0';
     fclose(input);
 
     // False if there aren't 54 tiles
-    if (strlen(cube) != 54) {
+    if (tilesCount != 54) {
         returnFalse();
     }
     // False if there aren't 9 tiles of each color
     int colors[6] = {0, 0, 0, 0, 0, 0};
-    for (int i = 0; i < strlen(cube); i++) {
-        if (cube[i] == 'R') {
+    for (int i = 0; i < tilesCount; i++) {
+        if (tiles[i] == 'R') {
             colors[0] += 1;
             if (colors[0] > 9) {
                 returnFalse();
             }
-        } else if (cube[i] == 'Y') {
+        } else if (tiles[i] == 'Y') {
             colors[1] += 1;
             if (colors[1] > 9) {
                 returnFalse();
             }
-        } else if (cube[i] == 'B') {
+        } else if (tiles[i] == 'B') {
             colors[2] += 1;
             if (colors[2] > 9) {
                 returnFalse();
             }
-        } else if (cube[i] == 'O') {
+        } else if (tiles[i] == 'O') {
             colors[3] += 1;
             if (colors[3] > 9) {
                 returnFalse();
             }
-        } else if (cube[i] == 'W') {
+        } else if (tiles[i] == 'W') {
             colors[4] += 1;
             if (colors[4] > 9) {
                 returnFalse();
             }
-        } else if (cube[i] == 'G') {
+        } else if (tiles[i] == 'G') {
             colors[5] += 1;
             if (colors[5] > 9) {
                 returnFalse();
@@ -80,51 +85,56 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Rearrange the string for simplicity
-    char temp[3];
-    memcpy(temp, &(cube[9]), 3);
-    memmove(&(cube[9]), &(cube[12]), strlen(cube) - 12);
-    memcpy(&(cube[strlen(cube) - 3]), temp, 3);
+    int cubieTileMatches[20][3] = {
+        // 1
+        {9, 0, 52},
+        // 2
+        {-1, 1, 53},
+        // 3
+        {17, 2, 54},
+        // 4
+        {10, 3, -1},
+        // 5
+        {16, 5, -1},
+        // 6
+        {11, 6, 12},
+        // 7
+        {-1, 7, 13},
+        // 8
+        {15, 8, 14},
+        // 9
+        {18, -1, 48},
+        // 10
+        {26, -1, 50},
+        // 11
+        {20, -1, 21},
+        // 12
+        {24, -1, 23},
+        // 13
+        {27, 42, 45},
+        // 14
+        {-1, 43, 46},
+        // 15
+        {35, 44, 47},
+        // 16
+        {28, 39, -1},
+        // 17 
+        {34, 41, -1},
+        // 18
+        {29, 36, 30},
+        // 19
+        {-1, 37, 31},
+        // 20
+        {33, 38, 32}
+    };
 
-    memcpy(temp, &(cube[15]), 3);
-    memmove(&(cube[15]), &(cube[18]), strlen(cube) - 18);
-    memcpy(&(cube[strlen(cube) - 3]), temp, 3);
-
-    memcpy(temp, &(cube[21]), 3);
-    memmove(&(cube[21]), &(cube[24]), strlen(cube) - 24);
-    memcpy(&(cube[strlen(cube) - 3]), temp, 3);
-
-    memcpy(temp, &(cube[15]), 3);
-    memmove(&(cube[15]), &(cube[12]), 3);
-    memcpy(&(cube[12]), temp, 3);
-
-    memcpy(temp, &(cube[21]), 3);
-    memmove(&(cube[18]), &(cube[15]), 6);
-    memcpy(&(cube[15]), temp, 3);
-
-    // Check to see if input is in goal state
-    if (strcmp(cube, GOAL) == 0) {
-        returnTrue();
+    unsigned char inputCube[20];
+    for (int i = 0; i < sizeof(cubieTileMatches)/sizeof(cubieTileMatches[0]); i++) {
+        inputCube[i] = generateCube(tiles, cubieTileMatches[i][0], cubieTileMatches[i][1], cubieTileMatches[i][2]);
     }
 
-    // Test for permutation parity
-    char currentTile;
-    char connectedTiles[3];
-    int home[3];
-    int result;
-    for (int i = 0; i < 54; i++) {
-        currentTile = cube[i];
-        if (currentTile != 'F') {
-            memcpy(connectedTiles, findConnectedTile(xcoor[i], ycoor[i], zcoor[i]), 2);
-            findHome(connectedTiles, home);
-            result = (home[0] - xcoor[0]) + (home[1] - ycoor[1]) + (home[2] - zcoor[2]);
-            if (result % 2 != 0) {
-                returnFalse();
-            }
-        }
-    }
-
-    returnTrue();
+    printf("Size of goal state: %lu\n", sizeof(goalState));
+    printf("Size of input cube: %lu\n", sizeof(inputCube));
 }
 
 // Function for exiting upon discovery of invalid cube
@@ -139,36 +149,84 @@ void returnTrue() {
     exit(0);
 }
 
-// Function for finding the connected tiles and thus the correct cubie
-char * findConnectedTile(int x, int y, int z) {
-    int count = 0;
-    static char coordinates[3];
-    for (int a = 0; a < 54; a++) {
-        for (int b = 0; b < 54; b++) {
-            for (int c = 0; c < 54; c++) {
-                if (a == b && b == c && xcoor[a] == x && ycoor[b] == y && zcoor[c] == z) {
-                    coordinates[count] = GOAL[a];
-                    count++;
-                }
-            }
-        }
+// Function to determine color of tile
+unsigned char tileColor(char t) {
+    if (t == 'R') {
+        return red;
+    } else if (t == 'G') {
+        return green;
+    } else if (t == 'Y') {
+        return yellow;
+    } else if (t == 'B') {
+        return blue;
+    } else if (t == 'O') {
+        return orange;
+    } else if (t == 'W') {
+        return white;
+    } else {
+        return none;
     }
-    return coordinates;
 }
 
-// Function for finding the goal state location of the cubie
-void findHome(char * connectedTiles, int home[]) {
-    if (sizeof(connectedTiles)/sizeof(connectedTiles[0]) < 3) {
-        for (int a = 0; a < 54; a++) {
-            for (int b = 0; b < 54; b++) {
-                if (GOAL[a] == connectedTiles[0] && GOAL[b] == connectedTiles[1]) {
-                    if(xcoor[a] == xcoor[b] && ycoor[a] == ycoor[b] && zcoor[a] == zcoor[b]) {
-                        home[0] = xcoor[a];
-                        home[1] = ycoor[a];
-                        home[2] = zcoor[a];
-                    }
-                }
-            }
+unsigned char z2TileColor(unsigned char x, unsigned char y, unsigned char z) {
+    unsigned char possible[2];
+    int possibleUsed = 0;
+    int i = 0;
+    int j = 0;
+    int length = sizeof(adjacency[0]) / sizeof(adjacency[0][0]);
+    while (i < length && j < length) {
+        if (adjacency[x-1][i] == adjacency[y-1][j]) {
+            possible[possibleUsed] = adjacency[x-1][i];
+            possibleUsed++;
+            i++;
+            j++;
+        } else if (adjacency[x-1][i] < adjacency[y-1][j]) {
+            i++;
+        } else {
+            j++;
         }
     }
+    if (z == possible[0]) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+unsigned char z4TileColor(unsigned char other, unsigned char z) {
+    for (unsigned char i = 0; i < sizeof(adjacency[0])/sizeof(adjacency[0][0]); i++) {
+        if (adjacency[other-1][i] == z) {
+            return i;
+        }
+    }
+    return 4;
+}
+
+unsigned char generateCube(char tiles[], int x, int y, int z) {
+
+    unsigned char x_color = 0;
+    unsigned char y_color = 0;
+    unsigned char z_color = 0;
+
+    if (x == -1) {
+        x_color = 0;
+        y_color = tileColor(tiles[y]);
+        z_color = tileColor(tiles[z]);
+        z_color = z4TileColor(y_color, z_color);
+    } else if (y == -1) {
+        x_color = tileColor(tiles[x]);
+        y_color = 0;
+        z_color = tileColor(tiles[z]);
+        z_color = z4TileColor(x_color, z_color);
+    } else if (z == -1) {
+        x_color = tileColor(tiles[x]);
+        y_color = tileColor(tiles[y]);
+        z_color = 0;
+    } else {
+        x_color = tileColor(tiles[x]);
+        y_color = tileColor(tiles[y]);
+        z_color = tileColor(tiles[z]);
+        z_color = z2TileColor(x_color, y_color, z_color);
+    }
+    return (x_color << 5) + (y_color << 2) + z_color;
 }
---
+++ solvable.h
@@ -0,0 +1,6 @@
+void returnFalse();
+void returnTrue();
+unsigned char tileColor(char t);
+unsigned char z2TileColor(unsigned char x, unsigned char y, unsigned char z);
+unsigned char z4TileColor(unsigned char other, unsigned char z);
+unsigned char generateCube(char tiles[], int x, int y, int z);
